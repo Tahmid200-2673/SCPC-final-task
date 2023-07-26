022a5d013fe1851c741c0c83c0c8bb03d529fcd6
Tahmid200-2673 <tahmidahmed22673@gmail.com>
2023-07-26 13:47:37 +0600

    Task3 finished

--- Task3.js
+++ Task3.js
@@ -1,3 +1,9 @@
+
+// Task 3: Write a JavaScript program to find the most frequent element in an array and return it. 
+
+// Example Input: [3, 5, 2, 5, 3, 3, 1, 4, 5] Example Output: 3
+
+
 function mostFrequent(arr) {
    let mf = 1;
    let m = 0;
